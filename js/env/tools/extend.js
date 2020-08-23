37553a5039d50c0f5962ffd7f734cfdbdbef5553
Aleksey Chichenkov <a.chichenkov@initi.ru>
2020-08-23 14:18:16 +0300

    update extend

--- js/env/tools/extend.js
+++ js/env/tools/extend.js
@@ -16,7 +16,7 @@
             if(_right === undefined) return target;
 
             for (var key in _right) {
-                if (_right.hasOwnProperty(key)) {
+                if (!_right.hasOwnProperty || _right.hasOwnProperty(key)) {
                     if (_right[key] instanceof Object && (_right[key] && _right[key].constructor === Object)) {
                         if (_left[key] && _left[key] instanceof Object)
                             target[key] = extend(_left[key], _right[key]);
