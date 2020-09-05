0ff551a351c905fc9809d256bf9d2a1127811535
Aleksey Chichenkov <a.chichenkov@initi.ru>
2020-09-05 17:59:44 +0300

    remove custom conf from repo

--- js/main.js
+++ js/main.js
@@ -6,49 +6,54 @@
 var deps = [
     "env/tools/standardTypeExtend",
     "conf/main",
-    "conf/custom",
     "api",
     "handlers",
     "env/tools/extend",
     "core/pageController",
-    // "core/pageExecutor",
     "core/pageLoader",
     "defaultLayout",
     "env/tabKeeper"
 ];
 require(deps, function() {
     var mainConf       = require("conf/main");
-    var customConf     = require("conf/custom");
     var API            = require("api");
     var handlers       = require("handlers");
     var extend         = require("env/tools/extend");
     var PageController = require("core/pageController");
-    // var PageExecutor   = require("core/pageExecutor");
     var PageLoader     = require("core/pageLoader");
 
-    window.config = extend(mainConf, customConf);
-
-    window.api = new API({
-        protocol: config.connection.socket.proto,
-        host: config.connection.socket.host,
-        port: config.connection.socket.port,
-        handlers: handlers
+    require(["conf/custom"], function (customConf) {
+        extend(mainConf, customConf);
+        initialize();
+    }, function () {
+        initialize();
     });
 
-    api.init();
+    var initialize = function () {
+        window.config = mainConf;
 
-    api.on("ready", function () {
-        pageExecutor = new PageLoader();
-
-        pageController = new PageController({
-            query: location.search
+        window.api = new API({
+            protocol: config.connection.socket.proto,
+            host: config.connection.socket.host,
+            port: config.connection.socket.port,
+            handlers: handlers
         });
-        pageController.run().then(function(_data) {
-            pageExecutor.load(_data.page);
-        }.bind(this));
 
-    });
+        api.init();
 
-    document.title = config.app.title;
+        api.on("ready", function () {
+            pageExecutor = new PageLoader();
+
+            pageController = new PageController({
+                query: location.search
+            });
+            pageController.run().then(function(_data) {
+                pageExecutor.load(_data.page);
+            }.bind(this));
+
+        });
+
+        document.title = config.app.title;
+    };
 });
 
