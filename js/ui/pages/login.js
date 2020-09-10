5446670f77b0847c87e12ce2d232fb1d8796afb4
DanSylvest <cublakhan257@gmail.com>
2020-09-10 12:04:15 +0300

    fix

--- js/ui/pages/login.js
+++ js/ui/pages/login.js
@@ -18,7 +18,7 @@ require(["env/cookie", "env/query"], function () {
           :md-content="dialogLoginDescription" />
 
     
-        <md-card style=" width: 450px" class="md-elevation-6">          
+        <md-card style="" class="md-elevation-6">          
         
             <md-card-content>
                 <md-tabs @md-changed="onTabChange" :md-active-tab="currentTab" md-dynamic-height>
