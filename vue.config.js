bebdcb25f1255c9ef03f562588a332d6a9cfb6a0
Aleksey Chichenkov <a.chichenkov@initi.ru>
2021-01-06 16:05:02 +0300

    remove vue conf

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Aleksey Chichenkov <rolahd@yandex.ru> on 10/13/20.
- */
-// var HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-    publicPath: '/tests/mapper/wanderer-client/dist/'
-}
