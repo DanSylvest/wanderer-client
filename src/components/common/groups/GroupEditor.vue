41fcd8aff43b053515ab23916aa54410d40cc0ce
DanSylvest <cublakhan257@gmail.com>
2021-07-25 14:11:23 +0300

    fix, update package.json

--- src/components/common/groups/GroupEditor.vue
+++ src/components/common/groups/GroupEditor.vue
@@ -139,7 +139,7 @@
                 let options = {
                     name: this.name,
                     description: this.description,
-                    characters: this.characters.map(x => parseInt(x)),
+                    characters: this.characters.map(x => x.toString()),
                     corporations: this.corporations,
                     alliances: this.alliances,
                     // moderators: this.moderators,
