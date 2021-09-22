6c815aca0cd7a1a077352f5382ac40606bb32a15
DanSylvest <cublakhan257@gmail.com>
2021-09-22 09:38:27 +0300

    Fix search

--- src/js/requests/getSearch.js
+++ src/js/requests/getSearch.js
@@ -1,29 +1,31 @@
-import axios from "axios";
-import {queryToString} from "../env/query";
+import axios from 'axios';
+import { queryToString } from '../env/query';
 
 /**
  *
- * @param categories {Array.<'character'|'corporation'|'alliance'>}
- * @param language
- * @param searchString
- * @param strict
- * @param datasource
+ * @param props.categories {Array.<'character'|'corporation'|'alliance'>}
+ * @param props.language
+ * @param props.searchString
+ * @param props.strict
+ * @param props.datasource
  * @returns {Promise<{
  *   character: Array.<number>
  * }>}
  */
-export const getSearch = async ({
-                                  categories = [],
-                                  language = 'en',
-                                  search = '',
-                                  strict = false,
-                                  datasource = 'tranquility'
-                                }) => {
+export const getSearch = async (props) => {
+  const {
+    categories = [],
+    language = 'en',
+    search = '',
+    strict = false,
+    datasource = 'tranquility',
+  } = props;
+
   try {
-    const opts = { datasource, language, categories: categories.join(','), search, strict }
-    const res = await axios.get(`http://esi.evetech.net/latest/search/?` + queryToString(opts));
+    const opts = { datasource, language, categories: categories.join(','), search, strict };
+    const res = await axios.get(`https://esi.evetech.net/latest/search/?` + queryToString(opts));
     return res.data;
   } catch (e) {
     console.error(e);
   }
-}
+};
