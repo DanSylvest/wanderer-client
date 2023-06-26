7763c26a57cfc9f3584871b7e59e6fa61629341b
DanSylvest <cublakhan257@gmail.com>
2023-06-26 20:20:49 +0300

    added ability to run with ssl

---
+++ scripts/runHostSSL.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const https = require('https');
+const fs = require('fs');
+
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+
+const port = process.env.HTTPS_PORT || 3000;
+
+console.log('SSL_CERT', process.env.SSL_PATH_CERT);
+console.log('SSL_KEY', process.env.SSL_PATH_KEY);
+
+// Загрузите SSL-сертификаты
+const options = {
+    key: fs.readFileSync(process.env.SSL_CERT),
+    cert: fs.readFileSync(process.env.SSL_KEY)
+};
+
+
+https.createServer(options, app).listen(port, () => {
+    console.log(`Server is listening on https://localhost:${port}`);
+});
